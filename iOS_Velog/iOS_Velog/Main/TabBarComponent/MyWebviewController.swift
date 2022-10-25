857724a619b149b52c072fe555e78f05e2f8f7df
Hongjunehuke <junhyeok2166@daum.net>
2022-10-25 23:28:55 +0900

    fix : make HomeView SwiftUI to UIKit

---
+++ iOS_Velog/iOS_Velog/Main/TabBarComponent/MyWebviewController.swift
@@ -0,0 +1,69 @@
+//
+//  MyWebview.swift
+//  iOS_Velog
+//
+//  Created by 홍준혁 on 2022/10/18.
+//
+
+//import SwiftUI
+//import WebKit
+//
+//// uikit 의 uiview 를 사용할수 있도록 한다.
+//// UIViewControllerRepresentable
+//
+//struct MyWebview: UIViewRepresentable {
+//
+//    var urlToLoad: String
+//
+//    // ui view 만들기
+//    func makeUIView(context: Context) -> WKWebView {
+//
+//        // unwrapping
+//        guard let url = URL(string: self.urlToLoad) else {
+//            return WKWebView()
+//        }
+//
+//        // 웹뷰 인스턴스 생성
+//        let webview = WKWebView()
+//
+//        // 웹뷰를 로드한다.
+//        webview.load(URLRequest(url: url))
+//
+//        return webview
+//
+//    }
+//
+//    // 업데이트 ui view
+//    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<MyWebview>) {
+//
+//       }
+//
+//}
+//
+//
+////struct MyWebview_Previews: PreviewProvider {
+////    static var previews: some View {
+////        MyWebview(urlToLoad: "https://www.naver.com")
+////    }
+////}
+
+
+import UIKit
+import WebKit
+class MyWebViewContoller: UIViewController {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://velog.io/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        }
+}
