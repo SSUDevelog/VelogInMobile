598f87f21c5dee87294a952d1aeefc62155de127
Hongjunehuke <junhyeok2166@daum.net>
2022-11-06 22:59:59 +0900

    feat: Login Server communicate

---
+++ iOS_Velog/iOS_Velog/Moya/SignInModel.swift
@@ -0,0 +1,21 @@
+//
+//  SignIn.swift
+//  iOS_Velog
+//
+//  Created by 홍준혁 on 2022/11/06.
+//
+
+import Foundation
+
+// MARK: - SignInModel
+struct SignInModel: Codable {
+    let code: Int
+    let msg: String
+    let success: Bool
+    let token: String
+}
+
+// MARK: - SignupResponse
+struct SigninResponse: Codable {
+    let id,password:String
+}
