df04791560c11bf16cd89ede727e8015630ef1ad
Hongjunehuke <junhyeok2166@daum.net>
2022-11-17 04:18:36 +0900

    fix: debug get subscribeList

---
+++ iOS_Velog/iOS_Velog/Main/TabBarComponent/SubScribe/userList.swift
@@ -0,0 +1,15 @@
+//
+//  userList.swift
+//  iOS_Velog
+//
+//  Created by 홍준혁 on 2022/11/17.
+//
+
+import Foundation
+struct userList{
+    static var List = [String]()
+    
+    init(list:[String]){
+        userList.List = list
+    }
+}
