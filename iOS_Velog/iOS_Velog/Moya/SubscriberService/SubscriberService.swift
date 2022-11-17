db16f7f3663953cf32874db84e0b53972f9ca138
Hongjunehuke <junhyeok2166@daum.net>
2022-11-18 02:40:16 +0900

    feat : search Velog ID & add to user SubscribeList

--- iOS_Velog/iOS_Velog/Moya/SubscriberService/SubscriberService.swift
+++ iOS_Velog/iOS_Velog/Moya/SubscriberService/SubscriberService.swift
@@ -9,10 +9,12 @@ import Foundation
 import Moya
 import Realm
 import RealmSwift
+import UIKit
 
 enum SubscriberService{
     case addSubscriber(param: AddRequest)
     case getSubscriber
+    case checkSubscriber(_ name: String)
 }
 
 extension SubscriberService: TargetType{
@@ -26,15 +28,19 @@ extension SubscriberService: TargetType{
             return "subscribe/addsubscriber"
         case .getSubscriber:
             return "subscribe/getsubscriber"
+        case .checkSubscriber(let name):
+            return "subscribe/inputname/\(name)"
         }
     }
-//http://34.125.230.85:8080/subscribe/addsubscriber?fcmToken=temporaryFCMToken&name=lhr4884
+
     
     var method: Moya.Method {
         switch self{
         case .addSubscriber:
             return .post
         case .getSubscriber:
+            return .get
+        case .checkSubscriber:
             return .get
         }
     }
@@ -45,6 +51,8 @@ extension SubscriberService: TargetType{
 //            return .requestJSONEncodable(param)   여기가 문제야!!!!
             return .requestParameters(parameters: ["fcmToken" : param.fcmToken,"name" : param.name], encoding: URLEncoding.queryString)
         case .getSubscriber:
+            return .requestPlain
+        case .checkSubscriber:
             return .requestPlain
         }
 
