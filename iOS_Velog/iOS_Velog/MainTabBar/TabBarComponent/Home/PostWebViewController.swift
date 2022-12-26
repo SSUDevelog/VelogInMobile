bab81d5cbbbd77b0f966841db62d2ebf082780db
Hongjunehuke <junhyeok2166@daum.net>
2022-12-26 16:08:29 +0900

    fix: Alert text

--- iOS_Velog/iOS_Velog/MainTabBar/TabBarComponent/Home/PostWebViewController.swift
+++ iOS_Velog/iOS_Velog/MainTabBar/TabBarComponent/Home/PostWebViewController.swift
@@ -254,9 +254,10 @@ class PostWebViewController: UIViewController {
                     present(alertForOut,animated: true,completion: nil)
                     
                 }else {
+                    // 구독한 사람이 아니면
                     let alert = UIAlertController(title: "구독자 추가", message: "해당 글을 쓴 유저를 구독하시겠습니까?", preferredStyle: UIAlertController.Style.alert)
                     
-                    let SuccessAlert = UIAlertController(title: "구독자 추가 되었습니다.", message: "구독자 리스트를 확인해주세요.", preferredStyle: UIAlertController.Style.alert)
+                    let SuccessAlert = UIAlertController(title: "성공", message: "구독자로 추가 되었습니다.", preferredStyle: UIAlertController.Style.alert)
                     
                     let okActionForSuccessAlert = UIAlertAction(title: "확인", style: .default,handler: {noAction in
                         return
@@ -294,13 +295,6 @@ class PostWebViewController: UIViewController {
             }
         }
         return true
-    }
-    
-    // 시간 딜레이 함수
-    public func delayWithSeconds(_ seconds: Double, completion: @escaping () -> ()) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
-            completion()
-        }
     }
     
     // 구독자 최종 추가
